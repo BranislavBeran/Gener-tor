c3c3542cf86e7de0d28a2e18e2d31c8627193019
BranislavBeran <branislav.beran@gmail.com>
2017-04-05 08:38:41 +0200

    Add files via upload

---
+++ generator.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main(){
+	
+	int *subor;
+	int n;
+	char meno[20];
+	
+	scanf("%s", &meno);
+	scanf("d",&n);
+	
+	subor = (int*)malloc ((4*n+1)*sizeof(unsigned char));
+	
+}
