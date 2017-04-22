4cf2b981276be430cfc30b34ec0807563effe52d
BranislavBeran <branislav.beran@gmail.com>
2017-04-23 00:51:20 +0200

    Update generator.cpp

--- generator.cpp
+++ generator.cpp
@@ -1,16 +1,73 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
 
 
+typedef struct{
+	unsigned char d;
+	float p[20];
+}POSTUPNOST;
+
+void generuj_postupnost(POSTUPNOST *A){
+	
+	int i;
+	
+	A->d=rand()%10+11;
+	
+	printf(" %d \n",A->d);
+	
+    for (i=0;i<A->d;i++){
+    	A->p[i]= rand()/(float)(RAND_MAX)* 20;
+    	
+	}
+}
+
+void vytvorsubor(char fileName[],int n){    
+	FILE *f;
+	int i,j;
+	POSTUPNOST A[n];
+	
+	f = fopen(fileName, "w");
+	if(f == NULL){
+		printf("%s", strerror(errno));
+		return;
+	}
+	
+	for(i=0;i<n;i++){
+		generuj_postupnost(&A[i]);
+	}
+//	
+//	for(i=0;i<n;i++){
+//		write(f,A[i].d,sizeof(unsigned char));		
+//		write(f,A[i].p,A[i].d*sizeof(float));
+//	}
+	
+
+	
+	if(fclose(f) == EOF){
+		printf("Unable to close file\n");
+		return;
+	}
+}
+	
 main(){
+	srand(time(NULL));
 	
-	int *subor;
-	int n;
+
+	int i;
 	char meno[20];
+	int n;
 	
-	scanf("%s", &meno);
-	scanf("d",&n);
+	scanf("%s",&meno);
+	scanf("%d",&n);
 	
-	subor = (int*)malloc ((4*n+1)*sizeof(unsigned char));
+
+	vytvorsubor(meno,n);
+
+//	subor = (int*)malloc (()*sizeof(unsigned char));
 	
 }
